2c5e6548a5f5735cc4fb1e42189a0addc9ca4fd2
Nathan <nathan.cakici@epitech.eu>
2026-01-19 19:05:50 +0100

    fix: clang-format

--- include/GameEngine/components/Sound.hpp
+++ include/GameEngine/components/Sound.hpp
@@ -9,20 +9,23 @@
 
 #include <string>
 
-namespace Components {
-    class Sound {
+namespace Components
+{
+    class Sound
+    {
         public:
             Sound();
-            Sound(const std::string &onSpawnSound, const std::string &onDestroySound);
+            Sound(const std::string& onSpawnSound, const std::string& onDestroySound);
             ~Sound();
 
             std::string getOnSpawnSound() const;
             std::string getOnDestroySound() const;
 
-            void setOnSpawnSound(const std::string &onSpawnSound);
-            void setOnDestroySound(const std::string &onDestroySound);
+            void setOnSpawnSound(const std::string& onSpawnSound);
+            void setOnDestroySound(const std::string& onDestroySound);
 
-            bool operator==(const Sound &other) const;
+            bool operator==(const Sound& other) const;
+
         protected:
         private:
             std::string _onSpawnSound;
--- src/components/Sound.cpp
+++ src/components/Sound.cpp
@@ -11,7 +11,8 @@ Components::Sound::Sound() : _onSpawnSound(""), _onDestroySound("")
 {
 }
 
-Components::Sound::Sound(const std::string &onSpawnSound, const std::string &onDestroySound) : _onSpawnSound(onSpawnSound), _onDestroySound(onDestroySound)
+Components::Sound::Sound(const std::string& onSpawnSound, const std::string& onDestroySound)
+    : _onSpawnSound(onSpawnSound), _onDestroySound(onDestroySound)
 {
 }
 
@@ -29,17 +30,17 @@ std::string Components::Sound::getOnDestroySound() const
     return _onDestroySound;
 }
 
-void Components::Sound::setOnSpawnSound(const std::string &onSpawnSound)
+void Components::Sound::setOnSpawnSound(const std::string& onSpawnSound)
 {
     _onSpawnSound = onSpawnSound;
 }
 
-void Components::Sound::setOnDestroySound(const std::string &onDestroySound)
+void Components::Sound::setOnDestroySound(const std::string& onDestroySound)
 {
     _onDestroySound = onDestroySound;
 }
 
-bool Components::Sound::operator==(const Sound &other) const
+bool Components::Sound::operator==(const Sound& other) const
 {
     return _onSpawnSound == other._onSpawnSound && _onDestroySound == other._onDestroySound;
 }
